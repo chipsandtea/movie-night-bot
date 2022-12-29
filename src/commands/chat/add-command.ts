bca48da73edd2392c09001b1eb37b085c0e30cb3
chipsandtea <hsiao.christopher@gmail.com>
2022-12-29 13:41:21 -0800

    Verify TMDB w undici works + begin adding models for TMDB.

--- src/commands/chat/add-command.ts
+++ src/commands/chat/add-command.ts
@@ -1,5 +1,7 @@
 import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
 import { RateLimiter } from 'discord.js-rate-limiter';
+import { createRequire } from 'node:module';
+import { Client } from 'undici';
 
 import { Language } from '../../models/enum-helpers/index.js';
 import { EventData } from '../../models/internal-models.js';
@@ -7,13 +9,33 @@ import { Lang } from '../../services/index.js';
 import { InteractionUtils } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
 
+const require = createRequire(import.meta.url);
+let Config = require('../../../config/config.json');
+
 export class AddCommand implements Command {
     public names = [Lang.getRef('chatCommands.add', Language.Default)];
     public cooldown = new RateLimiter(1, 5000);
     public deferType = CommandDeferType.PUBLIC;
     public requireClientPerms: PermissionsString[] = [];
+    public client = new Client(`https://api.themoviedb.org/`);
 
     public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
+        const reqHeaders = {
+            Authorization: `Bearer ${Config.tmdb.token}`,
+            'Content-Type': 'application/json;charset=utf-8',
+        };
+
+        // Verify v4 reader token pattern.
+        const { body, headers, statusCode, trailers } = await this.client.request({
+            path: '/3/movie/76341',
+            method: 'GET',
+            headers: reqHeaders,
+        });
+
+        const resp = await body.json();
+        console.log(resp);
+
+        console.log(body);
         await InteractionUtils.send(intr, Lang.getEmbed('displayEmbeds.add', data.lang));
     }
 }
